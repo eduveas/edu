d975b94fab9911fc722a5f5715aee5558da8651a
eduveas <eveas@know-center.at>
2023-12-16 04:17:18 +0100

    Add files via upload

---
+++ publications.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Eduardo Veas Publications</title>
+
+    <link rel="stylesheet" href="css/style-ev.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+
+	<section class="intro" id="zen-intro">
+		<!--header role="banner">
+			<h1>Eduardo Veas</h1>
+			<h2>The Beauty of <abbr title="Cascading Style Sheets">CSS</abbr> Design</h2>
+		</header-->
+
+		<div class="about-me" id="about-me">
+		  <div class="person-photo">
+		    <img src="media/eduardo.jpeg"></img>
+		  </div>
+
+		  
+		  <h3 class="">Eduardo Veas</h3>
+		  <p class="email"> eveas[at]tugraz[dot]at</p>
+		  <div class="address" id="address">
+		    <p>
+		      Interactive Systems and Data Science<br>
+		      Graz University of Technology<br>
+		      Inffeldgasse 13, 6th floor<br>
+		      Graz, Austria<br>
+		      8010<br>
+		    </p>
+		  </div>
+		  <div class="visitenkarte">
+		    <a href="https://online.tugraz.at/tug_online/visitenkarte.show_vcard?pPersonenId=8E739EC7D27607F7&pPersonenGruppe=3" title="Go to TUG visitenkarte">Visitenkarte @ TUG</a>
+		  </div>
+		</div>
+		  
+	</section>
+
+      
+      <section>
+        <h1>
+<a name="news" class="anchor" href="#news"><span class="octicon octicon-link"></span></a>Publications</h1>
+
+<script src="https://bibbase.org/show?bib=http%3A%2F%2Fkti.tugraz.at%2Fstaff%2Feveas%2Fpubs.tex&jsonp=1&jsonp=1"></script-->
+
+      </section>
+      <footer>
+        <!--p><small>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p-->
+      </footer>
+    </div>
+    <script src="javascripts/scale.fix.js"></script>
+
+  </body>
+
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58855117-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</html>
